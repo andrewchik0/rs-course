21ebaafe0787ac92eecb478db5e8176e06fce2ae
VasilevAndreiAV6 <vasilev6026@gmail.com>
2023-04-08 14:53:16 +0200

    refactor: fix test warnings

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -6,10 +6,14 @@ import cards from '../../assets/cards.json';
 
 describe('renders card', () => {
   it('renders card', () => {
-    /* eslint-disable @typescript-eslint/no-unused-vars */
-    const { ['birthday']: _, ...cardObject } = cards[0];
-    /* eslint-enable @typescript-eslint/no-unused-vars */
+    const { ['birthday']: stringDate, ...cardObject } = cards[0];
 
+    Object.defineProperties(cardObject, {
+      birthday: {
+        value: new Date(stringDate),
+        writable: true,
+      },
+    });
     render(<Card card={Object.assign(cardObject, { birthday: new Date() })} />);
 
     expect(screen.getByText(cards[0].name)).toBeInTheDocument();
@@ -21,10 +25,14 @@ describe('renders card', () => {
   });
 
   it('renders card', () => {
-    /* eslint-disable @typescript-eslint/no-unused-vars */
-    const { ['birthday']: _, ...cardObject } = cards[0];
-    /* eslint-enable @typescript-eslint/no-unused-vars */
+    const { ['birthday']: stringDate, ...cardObject } = cards[0];
 
+    Object.defineProperties(cardObject, {
+      birthday: {
+        value: new Date(stringDate),
+        writable: true,
+      },
+    });
     const now = new Date();
     const date = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate() - 4);
     render(<Card card={Object.assign(cardObject, { birthday: date })} />);
@@ -32,10 +40,14 @@ describe('renders card', () => {
   });
 
   it('renders card', () => {
-    /* eslint-disable @typescript-eslint/no-unused-vars */
-    const { ['birthday']: _, ...cardObject } = cards[0];
-    /* eslint-enable @typescript-eslint/no-unused-vars */
+    const { ['birthday']: stringDate, ...cardObject } = cards[0];
 
+    Object.defineProperties(cardObject, {
+      birthday: {
+        value: new Date(stringDate),
+        writable: true,
+      },
+    });
     const now = new Date();
     const date = new Date(now.getFullYear() - 1, now.getMonth(), now.getDate());
     render(<Card card={Object.assign(cardObject, { birthday: date })} />);
